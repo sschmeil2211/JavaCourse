4fcbbd15af26cc6c49c4b36afd6b4e0a22ca3fd7
sschmeil22 <sebaschmeil123@gmail.com>
2023-04-24 01:14:33 -0300

    first commit

---
+++ src/com/primerejercicio/PrimerPunto.java
@@ -0,0 +1,29 @@
+package com.primerejercicio;
+
+public class PrimerPunto {
+
+    public static void main(String[] args){
+
+        boolean varBoolean = true;
+        byte varByte = 1;
+        char varChar = 'a';
+        double varDouble = 1.2d;
+        float varFloat = 1.3f;
+        int varInt = 2;
+        long varLong = 100;
+        short varShort = 10;
+        String varString = "PrimerPunto";
+
+        System.out.printf(
+            "Bool   : " + varBoolean +
+            "\nByte   : " + varByte +
+            "\nChar   : " + varChar +
+            "\nDouble : " + varDouble +
+            "\nFloat  : " + varFloat +
+            "\nInt    : " + varInt +
+            "\nLong   : " + varLong +
+            "\nShort  : " + varShort +
+            "\nString : " + varString
+        );
+    }
+}
